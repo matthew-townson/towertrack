5c3c095530d96c81187ce98e0f31e065d01f6a05
Matthew Townson <mjwtownson@gmail.com>
2025-09-05 13:14:16 +0100

    replaced with analyze
    avoids long metadata locks

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -196,17 +196,17 @@ async function initialiseDatabase() {
         try {
             console.log('[ INFO ] Attempt to optimise db tables')
             console.log('         ├ Optimising User table');
-            await connection.query(`OPTIMIZE TABLE \`User\``);
+            await connection.query(`ANALYZE TABLE \`User\``);
             console.log('         ├ Optimising UserSettings table');
-            await connection.query(`OPTIMIZE TABLE \`UserSettings\``);
+            await connection.query(`ANALYZE TABLE \`UserSettings\``);
             console.log('         ├ Optimising Tower table');
-            await connection.query(`OPTIMIZE TABLE \`Tower\``);
+            await connection.query(`ANALYZE TABLE \`Tower\``);
             console.log('         ├ Optimising Bell table');
-            await connection.query(`OPTIMIZE TABLE \`Bell\``);
+            await connection.query(`ANALYZE TABLE \`Bell\``);
             console.log('         ├ Optimising Performance table');
-            await connection.query(`OPTIMIZE TABLE \`Performance\``);
+            await connection.query(`ANALYZE TABLE \`Performance\``);
             console.log('         └ Optimising Log table');
-            await connection.query(`OPTIMIZE TABLE \`Log\``);
+            await connection.query(`ANALYZE TABLE \`Log\``);
             console.log('[ SUCCESS ] Tables optimized successfully');
         } catch (error) {
             console.error('[ ERROR ] Failed to optimize tables:', error.message);
